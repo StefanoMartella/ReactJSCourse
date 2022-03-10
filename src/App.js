7519ff2ebb34ccf7a16648dfc44d06fd9df9bfff
StefanoMartella <stefano.martella9614@gmail.com>
2022-03-10 10:59:11 +0100

    Add forms

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 import AboutPage from "./components/pages/AboutPage";
 import ContactPage from "./components/pages/ContactPage";
 import Footer from "./components/pages/elements/Footer";
@@ -7,23 +8,29 @@ import Navbar from "./components/pages/elements/Navbar";
 import HomePage from "./components/pages/HomePage";
 import ProfilePage from "./components/pages/ProfilePage";
 import UserPage from "./components/pages/UserPage";
-import "./App.css";
+
+const NotFoundPage = lazy(() => import("./components/pages/NotFoundPage"));
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/profile">
-            <Route index element={<ProfilePage />} />
-            <Route path=":username" element={<UserPage />} />
-          </Route>
-        </Routes>
-        <Footer />
+        <Suspense
+          fallback={<h1 className="text-danger bg-danger">Caricamento...</h1>}
+        >
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/profile">
+              <Route index element={<ProfilePage />} />
+              <Route path=":username" element={<UserPage />} />
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+          <Footer />
+        </Suspense>
       </BrowserRouter>
     );
   }
---
+++ src/components/pages/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+function NotFoundPage() {
+  return (
+    <main className="vh-100">
+      <h1>404 - Not found</h1>
+    </main>
+  );
+}
+
+export default NotFoundPage;
